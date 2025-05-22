3f5ff41a4a2618869152c4f10485e9b712b8ed4a
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-05-22 14:21:05 -0400

    UPDATE: Project Configuration & Dependencies (Debugging)

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+// Add CSS support
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'css'];
+
+module.exports = config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     './App.{js,jsx,ts,tsx}',
     './routes/**/*.{js,jsx,ts,tsx}',
     './screens/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
+    './index.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
   ],
-  presets: [require('nativewind/preset')],
-  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
+  important: 'html',
+  corePlugins: {
+    preflight: false,
+  },
 };
