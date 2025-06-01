fc779ba1b2c8d78c11e264c662cbc40c1a63251f
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-06-01 19:13:44 -0400

    UPDATE: Handle Light/Dark Mode Throughout

--- App.tsx
+++ App.tsx
@@ -14,16 +14,16 @@ import '@/global.css';
 NativeWindStyleSheet.create({});
 
 const AppContent = () => {
-	const { theme } = useTheme();
+	const { isDarkMode } = useTheme();
 
 	return (
-		<SafeAreaView className={`App flex-1 bg-background ${theme}`}>
+		<SafeAreaView className={`App flex-1 ${isDarkMode ? 'dark' : ''}`}>
 			<View className='mx-auto w-full max-w-4xl flex-1'>
 				<Header />
 				<Body />
 				<Footer />
 			</View>
-			<StatusBar style={theme === 'dark' ? 'light' : 'dark'} />
+			<StatusBar style={isDarkMode ? 'light' : 'dark'} />
 		</SafeAreaView>
 	);
 };
