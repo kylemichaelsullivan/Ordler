f7ebc3201b9931fffbec9533c91b6291d6b27709
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-06-02 20:26:41 -0400

    UPDATE: Styling for More Control

--- components/body/results/SelectMore.tsx
+++ components/body/results/SelectMore.tsx
@@ -1,13 +1,17 @@
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 
 const SelectMore = () => (
-	<Text className='SelectMore has-black-color has-dark-fill flex-1 items-center justify-center text-center'>
-		Select 2 or More <FontAwesomeIcon icon={faThumbsUp} color='currentColor' /> or{' '}
-		<FontAwesomeIcon icon={faThumbsDown} color='currentColor' />
-	</Text>
+	<View className='SelectMore has-black-color has-dark-fill flex-row flex-1 items-center justify-center gap-2'>
+		<Text className='text-center'>Select 2 or More</Text>
+		<View className='flex-row items-center gap-2'>
+			<FontAwesomeIcon icon={faThumbsUp} color='currentColor' />
+			<Text>or</Text>
+			<FontAwesomeIcon icon={faThumbsDown} color='currentColor' />
+		</View>
+	</View>
 );
 
 export default SelectMore;
