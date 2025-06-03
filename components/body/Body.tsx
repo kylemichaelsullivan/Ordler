723ca5e952ad007ae03648d46d7927a4e99f2d6d
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-06-02 20:23:39 -0400

    UPDATE: Optimize with Memo

--- components/body/Body.tsx
+++ components/body/Body.tsx
@@ -1,19 +1,20 @@
+import { memo } from 'react';
 import { ScrollView, View } from 'react-native';
 
 import Letters from './letters/Letters';
 import Word from './word/Word';
 import Results from './results/Results';
 
-const Body = () => {
+const Body = memo(() => {
 	return (
 		<ScrollView className='Body flex-1'>
-			<View className='has-white-background-color flex-1 w-full gap-6 p-4'>
+			<View className='has-white-background-color flex-1 w-full gap-6 px-2 py-4'>
 				<Letters />
 				<Word />
 				<Results />
 			</View>
 		</ScrollView>
 	);
-};
+});
 
 export default Body;
--- components/body/letters/Letters.tsx
+++ components/body/letters/Letters.tsx
@@ -1,17 +1,45 @@
-import { View } from 'react-native';
+import { memo } from 'react';
+import { FlatList, View, useWindowDimensions } from 'react-native';
 
 import Letter from './Letter';
 
-import { letters } from '@/types/ordler';
+import { letters, type Letter as LetterType } from '@/types/ordler';
 
-const Letters = () => {
+const Letters = memo(() => {
+	const { width } = useWindowDimensions();
+	const numColumns =
+		width < 350 ? 2
+		: width < 475 ? 3
+		: width < 575 ? 4
+		: width < 700 ? 5
+		: width < 800 ? 6
+		: 7;
+
+	const renderItem = ({ item: letter }: { item: LetterType }) => <Letter letter={letter} />;
+
 	return (
-		<View className='Letters grid grid-cols-4 gap-4 py-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10'>
-			{letters.map(letter => (
-				<Letter key={letter} letter={letter} />
-			))}
+		<View className='Letters w-full'>
+			<FlatList
+				key={numColumns}
+				data={letters}
+				renderItem={renderItem}
+				keyExtractor={item => item}
+				numColumns={numColumns}
+				className='w-full'
+				contentContainerStyle={{
+					gap: 16,
+				}}
+				columnWrapperStyle={{
+					gap: 16,
+					justifyContent: 'center',
+				}}
+				showsVerticalScrollIndicator={false}
+				initialNumToRender={10}
+				maxToRenderPerBatch={10}
+				windowSize={5}
+			/>
 		</View>
 	);
-};
+});
 
 export default Letters;
